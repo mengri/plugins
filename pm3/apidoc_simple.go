1a7d67ec96851ebb7eca29dd9e1b8a6942a383a6
黄孟柱 <7427565+huang_meng_zhu@user.noreply.gitee.com>
2025-10-28 10:15:02 +0800

    simple

--- pm3/apidoc_simple.go
+++ pm3/apidoc_simple.go
@@ -3,10 +3,12 @@ package pm3
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/mengri/plugins/pm3/binding"
 	"log"
 	"net/http"
 )
 
+// 生成无输入的api
 func CreateApiSimple[OUTPUT any](method string, path string, handler func(ctx *gin.Context) (OUTPUT, error)) Api {
 	if handler == nil {
 		log.Fatalf("handler is nil for %s %s", method, path)
@@ -30,5 +32,36 @@ func GenSimpleHandler[OUTPUT any](handler func(ctx *gin.Context) (OUTPUT, error)
 		}
 
 		Response(ctx, output)
+	}
+}
+
+// 生成无响应的api( 仅响应状态)
+func CreateApiNone[INPUT any](method string, path string, handler func(ctx *gin.Context, input *INPUT) error) Api {
+	if handler == nil {
+		log.Fatalf("handler is nil for %s %s", method, path)
+		return nil
+	}
+	h := GenNoneHandler(handler)
+	return &formApi{
+		method:  method,
+		path:    path,
+		handler: h,
+	}
+}
+
+func GenNoneHandler[INPUT any](handler func(ctx *gin.Context, input *INPUT) error) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var input = new(INPUT)
+		if err := binding.AutoBind(ctx, &input); err != nil {
+			ResponseErr(ctx, http.StatusBadRequest, fmt.Sprintf("invald request:%s", err.Error()))
+			return
+		}
+		err := handler(ctx, input)
+		if err != nil {
+			ResponseErr(ctx, http.StatusInternalServerError, fmt.Sprintf("server error:%s", err.Error()))
+			return
+		}
+
+		ResponseOk(ctx)
 	}
 }
